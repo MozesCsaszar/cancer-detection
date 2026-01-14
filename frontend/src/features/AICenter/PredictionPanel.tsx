0ca9a2a2f900f7112dddc33da394a32163c16d4b
MozesCsaszar <mozescsaszar@gmail.com>
2026-01-14 22:46:18 +0200

    fix: reset prediction on new model trained

--- frontend/src/features/AICenter/PredictionPanel.tsx
+++ frontend/src/features/AICenter/PredictionPanel.tsx
@@ -63,6 +63,8 @@ const PredictionPanel: FC<PredictionPanelProps> = ({
   }>(createInputValues(trainingResult));
 
   useEffect(() => {
+    setPrediction(null);
+
     setInputValues(createInputValues(trainingResult));
   }, [trainingResult, setInputValues]);
 
