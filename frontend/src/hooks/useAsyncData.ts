e767830affabca1a891bd68f6ffc0d02255ebe75
MozesCsaszar <mozescsaszar@gmail.com>
2026-01-14 22:56:48 +0200

    feat: update data matching criterion for when to update data

--- frontend/src/hooks/useAsyncData.ts
+++ frontend/src/hooks/useAsyncData.ts
@@ -2,6 +2,19 @@ import { useEffect, useState } from "react";
 import { type DataEntry } from "../domain/entries";
 import API from "../api/csvAPI";
 
+function doesDataMatch(oldData: DataEntry[], newData: DataEntry[]) {
+  if (oldData.length === 0) {
+    return newData.length === 0;
+  }
+
+  const oldDataIDs = new Set(oldData.map((entry) => entry.ID));
+  const newDataIDs = new Set(newData.map((entry) => entry.ID));
+  return (
+    oldDataIDs.size === newDataIDs.size &&
+    Array.from(oldDataIDs).filter((id) => !newDataIDs.has(id)).length === 0
+  );
+}
+
 export function useAsyncData() {
   const [data, setData] = useState<DataEntry[]>([]);
   const [loading, setLoading] = useState(true);
@@ -16,7 +29,11 @@ export function useAsyncData() {
       API.getData()
         .then(([respData, error]) => {
           if (error === "") {
-            setData(respData);
+            const dataMatches = doesDataMatch(data, respData);
+            console.log(dataMatches, data.length, respData.length);
+            if (!dataMatches) {
+              setData(respData);
+            }
           } else {
             setError(error);
           }
